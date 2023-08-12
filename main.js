c4b227dd984221624bfba547b84be749d22191de
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-08-12 11:06:07 +1000

    Moved javascript code to main.js and deleted the associated js scripts in index.html

---
+++ main.js
@@ -0,0 +1,6 @@
+function changeBackgroundColor() {
+    document.getElementById("divsection").style.backgroundColor = "black";
+ }
+ function changeBackgroundColor2() {
+    document.getElementById("divsection2").style.backgroundColor = "white";
+ }
